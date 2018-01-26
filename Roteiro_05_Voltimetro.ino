4c3cd1970235fb433e07ffc7eb87119f5c32aaf0
Andre Bahiense <andregbahiense@gmail.com>
2018-01-25 23:48:47 -0300

    Add files via upload

---
+++ Roteiro_05_Voltimetro.ino
@@ -0,0 +1,22 @@
+const int analogInPin = A0;   
+
+float sensorValue = 0;        
+float outputValue = 0;
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+
+void loop() {
+sensorValue = analogRead(analogInPin);                                               
+outputValue = (sensorValue*5)/1023;      
+
+  Serial.print("sensor = " );                       
+  Serial.print(sensorValue);      
+  Serial.print("\t output = ");      
+  Serial.println(outputValue,4);   
+
+  delay(2);                     
+}
+
