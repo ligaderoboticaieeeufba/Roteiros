ddc5cbfaf465b4563a064ce5818c75fc55a10129
Andre Bahiense <andregbahiense@gmail.com>
2018-01-25 23:48:15 -0300

    Roteiro 4 - Pisca LED Ajustável

--- Roteiro_04_Pisca_LED.ino
+++ Roteiro_04_Pisca_LED.ino
@@ -1,4 +1,9 @@
-//Roteiro 4
+/*
+OBJETIVO
+Criar um sistema que possa ajustar a tempo entre o estado ativo/inativo do led com um
+potenciômetro.
+*/
+
 void setup() {
 pinMode(2,OUTPUT);
 pinMode(A1,INPUT);
