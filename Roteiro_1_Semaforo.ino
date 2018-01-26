3d5a8f6e13de777885fc37db2e1f7080fb897af2
Andre Bahiense <andregbahiense@gmail.com>
2018-01-25 23:46:10 -0300

    Roteiro 1 - Semáforo

--- Roteiro_1_Semaforo.ino
+++ Roteiro_1_Semaforo.ino
@@ -7,23 +7,23 @@ segundos respectivamente.
 #define ledvermelho 12
 #define ledamarelo 11
 #define ledverde 10
+
 void setup() {
   // Configurando pino como saida
-pinMode(ledvermelho,OUTPUT);
-pinMode(ledamarelo,OUTPUT);
-pinMode(ledverde,OUTPUT);
-
+pinMode (ledvermelho, OUTPUT);
+pinMode (ledamarelo, OUTPUT);
+pinMode (ledverde, OUTPUT);
 }
 
 void loop() {
-digitalWrite(ledverde,HIGH);
-delay(5000);//espera 5 segundos
-digitalWrite(ledverde,LOW);
-digitalWrite(ledamarelo,HIGH);
-delay(1000);//espera 1 segundos
-digitalWrite(ledamarelo,LOW);
-digitalWrite(ledvermelho,HIGH);
-delay(3000);//espera 3 segundos
-digitalWrite(ledvermelho,LOW);
-digitalWrite(ledverde,HIGH);
+digitalWrite (ledverde, HIGH);
+delay (5000); //espera 5 segundos
+digitalWrite (ledverde, LOW);
+digitalWrite (ledamarelo, HIGH);
+delay (1000); //espera 1 segundos
+digitalWrite (ledamarelo, LOW);
+digitalWrite (ledvermelho, HIGH);
+delay (3000); //espera 3 segundos
+digitalWrite (ledvermelho, LOW);
+digitalWrite (ledverde, HIGH);
 }
